8d14215a8007d7dc3b75e799ac32f31a954de8b9
Jonathon Hellmann <jonatho7@vt.edu>
2015-02-03 22:29:54 -0500

    Categorized the html examples into 4 categories
    (So that they would be easier to understand)

---
+++ Ex 2 - GeoJSON - Points with circle styling/pointOne.js
@@ -0,0 +1,50 @@
+var pointOneJSON =
+{
+    "type": "FeatureCollection",
+    "metadata": {
+        "generated": 1421513434000,
+        "url": "http://earthquake.usgs.gov/earthquakes/feed/geojsonp/2.5/week",
+        "title": "USGS Magnitude 2.5+ Earthquakes, Past Week",
+        "subTitle": "Real-time, worldwide earthquake list for the past week",
+        "cacheMaxAge": 60
+    },
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "mag": 5,
+                "place": "108km SSE of Tondano, Indonesia",
+                "time": "1421510860310",
+                "updated": "1421511701600",
+                "tz": 480,
+                "url": "http://earthquake.usgs.gov/earthquakes/eventpage/usc000tgru",
+                "felt": 0,
+                "cdi": 1,
+                "mmi": null,
+                "alert": null,
+                "status": "reviewed",
+                "tsunami": null,
+                "sig": "385",
+                "net": "us",
+                "code": "c000tgru",
+                "ids": ",usc000tgru,",
+                "sources": ",us,",
+                "types": ",cap,dyfi,general-link,geoserve,nearby-cities,origin,phase-data,tectonic-summary,",
+                "nst": null,
+                "dmin": "2.01",
+                "rms": "1.29",
+                "gap": "40",
+                "magnitudeType": "mb"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    125.3825,
+                    0.4431,
+                    64.42
+                ]
+            },
+            "id": "usc000tgru"
+        }
+    ]
+};
---
+++ Ex 2 - GeoJSON - Points with circle styling/pointTwo.js
@@ -0,0 +1,87 @@
+var pointTwoJSON =
+{
+    "type": "FeatureCollection",
+    "metadata": {
+        "generated": 1421513434000,
+        "url": "http://earthquake.usgs.gov/earthquakes/feed/geojsonp/2.5/week",
+        "title": "USGS Magnitude 2.5+ Earthquakes, Past Week",
+        "subTitle": "Real-time, worldwide earthquake list for the past week",
+        "cacheMaxAge": 60
+    },
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "mag": 2.5,
+                "place": "11km NW of Honoka'a, Hawaii",
+                "time": "1421506213000",
+                "updated": "1421506458003",
+                "tz": -600,
+                "url": "http://earthquake.usgs.gov/earthquakes/eventpage/hv60835146",
+                "felt": 0,
+                "cdi": 1,
+                "mmi": null,
+                "alert": null,
+                "status": "automatic",
+                "tsunami": null,
+                "sig": "96",
+                "net": "hv",
+                "code": "60835146",
+                "ids": ",hv60835146,",
+                "sources": ",hv,",
+                "types": ",dyfi,general-link,geoserve,nearby-cities,origin,",
+                "nst": null,
+                "dmin": "0.233562",
+                "rms": "0.18",
+                "gap": "233.999981280001",
+                "magnitudeType": "md"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -155.536,
+                    20.1692,
+                    42.8
+                ]
+            },
+            "id": "hv60835146"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "mag": 4.4,
+                "place": "11km SW of Yasuj, Iran",
+                "time": "1421494928660",
+                "updated": "1421496349635",
+                "tz": 210,
+                "url": "http://earthquake.usgs.gov/earthquakes/eventpage/usc000tgqm",
+                "felt": 0,
+                "cdi": 1,
+                "mmi": null,
+                "alert": null,
+                "status": "reviewed",
+                "tsunami": null,
+                "sig": "298",
+                "net": "us",
+                "code": "c000tgqm",
+                "ids": ",usc000tgqm,",
+                "sources": ",us,",
+                "types": ",cap,dyfi,general-link,geoserve,nearby-cities,origin,phase-data,tectonic-summary,",
+                "nst": null,
+                "dmin": "6.998",
+                "rms": "0.88",
+                "gap": "76",
+                "magnitudeType": "mb"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    51.5066,
+                    30.5951,
+                    10
+                ]
+            },
+            "id": "usc000tgqm"
+        }
+    ]
+};
